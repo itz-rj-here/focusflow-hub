df4e7c4a51277a5dfd9d86b459a9d2e6d9acb2a0
FocusFlow Developer <dev@focusflow.app>
2026-05-03 23:41:55 +0600

    Update worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,10 @@
-import { createServerEntry } from "./dist/server/index.js";
-
 export default {
   async fetch(request, env, ctx) {
-    return await createServerEntry.fetch(request, { env }, ctx);
+    try {
+      const { createServerEntry } = await import("./dist/server/index.js");
+      return await createServerEntry.fetch(request, { env }, ctx);
+    } catch (e) {
+      return new Response("Error: " + e.message, { status: 500 });
+    }
   },
 };
